610d69831405732fb1830a8621319d61bd6971b3
José Ignacio Villanueva Rojas <jose-ignacio.villanu@seres.int>
2018-10-17 15:20:53 +0200

    Adding WS projects

---
+++ CrunchifyWS/src/crunchify/com/web/service/CrunchifyHelloWorld.java
@@ -0,0 +1,13 @@
+package crunchify.com.web.service;
+
+public class CrunchifyHelloWorld {
+
+	public float addValue(float value) {
+		return (value + 10);
+	}
+ 
+	public float subtractValue(float value) {
+		return (value - 10);
+	}
+	
+}
